f5cc25e30ca61c115b0c7cae7c677b572303942f
f_o_x_l_v_i_v <burco.ab@gmail.com>
2019-12-16 02:02:45 +0200

    Create MVC project

---
+++ WebShop/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            var category = new List<Category>
+            {
+                new Category {Name = "Electronics"},
+                new Category {Name = "Furniture"},
+                new Category {Name = "Accessories"}
+            };
+            return View(category);
+            //return "Hello from index";
+        }
+
+        public ActionResult Browse(string category)
+        {
+            var categoryModel = new Category { Name = category };
+            return View(categoryModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var item = new Item { Title = "Item" + id };
+            return View(item);
+        }
+    }
+}
---
+++ WebShop/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public Category Category { get; set; }
+    }
+}
